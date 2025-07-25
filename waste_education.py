b05b3b2631c8c3b716b08fb8208827715c0d2807
NkemJefersonAchia <nkemachia05@gmail.com>
2025-07-25 14:49:02 +0200

    Sylivie's work

--- waste_education.py
+++ waste_education.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Display waste education tips."""
+"""Display two random waste education tips."""
 
 from db import DBBase
 
@@ -7,8 +7,10 @@ class WasteEducation(DBBase):
     def educate(self):
         with self.conn() as conn:
             cur = conn.cursor()
-            cur.execute("SELECT tip FROM tips")
+            # Select 2 random tips from the tips table
+            cur.execute("SELECT tip FROM tips ORDER BY RAND() LIMIT 2")
             rows = cur.fetchall()
+
         print("\nWaste Education Tips:")
         for (tip,) in rows:
             print("- " + tip)
